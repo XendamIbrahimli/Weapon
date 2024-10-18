8a5b156e45db0ffc7f3485a1a73c254b2382e7e4
BextiyarNovruzov <memmedlixedice318@gmail.com>
2024-10-18 16:41:22 +0400

    First

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿namespace New
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity;
+            int count;
+
+            while (true)
+            {
+                Console.WriteLine("Please enter capacity of weapon:");
+                string capacity1=Console.ReadLine();
+
+                if(int.TryParse(capacity1, out capacity) && capacity > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("The capacity isnot true value.");
+                }
+            }
+
+
+            while (true)
+            {
+                Console.WriteLine("Please enter count of weapon:");
+                string count1 = Console.ReadLine();
+
+                if (int.TryParse(count1, out count) && count > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("The count isnot true value.");
+                }
+            }
+
+            Console.WriteLine("Please enter mod of weapon:");
+            string mod =Console.ReadLine();
+
+            Weapon weapon=new Weapon(capacity, count, mod);
+
+            //weapon.ChangeFireMode();
+            //weapon.Shoot();
+            //Console.WriteLine(weapon.GetRemainBulletCount());
+            //weapon.Reload();
+
+
+            while (true) 
+            {
+                Console.WriteLine("0 - Get info");
+                Console.WriteLine("1 - For Shoot method");
+                Console.WriteLine("2 - For GetRemainBulletCount method");
+                Console.WriteLine("3 - For Reload method");
+                Console.WriteLine("4 - For ChangeFireMode method");
+                Console.WriteLine("5 - Stop");
+
+
+                Console.WriteLine();
+                string input=Console.ReadLine();
+
+                Console.WriteLine();
+
+                switch (input) 
+                {
+                    case "0":
+                        Console.WriteLine($"{weapon.Capacity} {weapon.Count} {weapon.Mod}");
+                        break;
+
+                    case "1":
+                        weapon.Shoot();
+                        break;
+                    
+                    case "2":
+                        Console.WriteLine(weapon.GetRemainBulletCount());
+                        break;
+
+                    case "3":
+                        weapon.Reload();
+                        break;
+
+                    case "4":
+                        weapon.ChangeFireMode();
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Stop");
+                        return;
+
+                    default:
+                        Console.WriteLine("incorrect choose.");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New
+{
+    internal class Weapon
+    {
+        private int _capacity;
+        private int _count;
+        private string _mod;
+
+        public Weapon(int capacity, int count, string mode)
+        {
+            Capacity = capacity;
+            Count = count;
+            Mod = mode;
+
+            
+
+        }
+
+        public int Capacity 
+        {
+            get 
+            { 
+                return _capacity;
+
+            }
+            set 
+            {
+                if (value > 0)
+                {
+                    _capacity = value;
+
+                }
+                else 
+                {
+                    Console.WriteLine("incorrect choose");
+                    Environment.Exit(0);
+                    
+                }
+
+            } 
+        }
+        public int Count 
+        { 
+            get 
+            { 
+                return _count;
+
+            } 
+            set 
+            {
+                if (value<=Capacity) 
+                { 
+                    _count = value;
+                }
+                else 
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Error.");
+                    Environment.Exit(0);
+
+                }
+            } 
+        }
+        public string Mod {
+            get 
+            { 
+                return _mod;
+
+            }
+
+            set 
+            {
+                if (value=="single" || value =="auto") 
+                {
+                    _mod = value;
+                }
+            } 
+        }
+
+        public void Shoot()
+        {
+
+            if (Mod=="single")
+            {
+                Count--;
+                if (Count >= 0)
+                {
+                    Console.WriteLine(Count);
+                }
+                else
+                {
+                    Console.WriteLine("Count cannot be equal neagative number.");
+                    Environment.Exit(0);
+                }
+            }
+            else if(Mod=="auto")
+            {
+                Count=0;
+                Console.WriteLine(Count);
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            int num = 0;
+            num=Capacity-Count;
+            return num;
+
+        }
+
+        public void Reload()
+        {
+            
+            Count = Capacity;
+            Console.WriteLine(Count);
+        }
+
+        public void ChangeFireMode()
+        {
+            if (Mod == "single")
+            {
+                Mod = "auto";
+
+            }
+            else if (Mod == "auto")
+            {
+                Mod = "single";
+            }
+        }
+
+
+    }
+}
